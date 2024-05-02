73a4ae88fac17e6131dec6da0cbb26eba819e879
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-05-02 12:34:47 +0200

    Add: Header section added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./sections/Header/Header";
 
 export default function App() {
     return (
         <div className="app">
             <Navbar />
+            <Header />
         </div>
     );
 }
