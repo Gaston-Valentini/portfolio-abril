2407c519950db7f7185f81a2c330611f1f41b423
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-05-02 11:57:40 +0200

    Add: Navbar component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <Navbar />
+        </div>
+    );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import style from "./Navbar.module.css";
+import { RiMenuUnfoldLine } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const switchOpen = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className={style.container}>
+            <div className={style.logo}>Abril Valentini</div>
+            <RiMenuUnfoldLine className={!isOpen ? style.bars : `${style.bars} ${style.barsOpen}`} onClick={switchOpen} />
+            <div className={!isOpen ? style.overlay : `${style.overlay} ${style.overlayOpen}`}>
+                <ul className={!isOpen ? style.links : `${style.links} ${style.linksOpen}`}>
+                    <IoMdClose className={style.linksClose} onClick={switchOpen} />
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={switchOpen}>
+                            Inicio
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={switchOpen}>
+                            Servicios
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={switchOpen}>
+                            Preguntas frecuentes
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={switchOpen}>
+                            Contacto
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
